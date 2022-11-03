9a13aff9b21fba7b66ad3c649ebea8d5448b8357
Mateo <mateoc09@gmial.com>
2022-11-03 17:59:45 -0500

    Main activities initial design.

---
+++ app/src/main/java/com/my_pet/yourperfectpet/CreatePetActivity.java
@@ -0,0 +1,14 @@
+package com.my_pet.yourperfectpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatePetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_pet);
+    }
+}
